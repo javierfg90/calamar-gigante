cfbda67db104e9c6caf1b961615d96d2eb0e8aab
Javier <javierfg90@nauta.cu>
2019-11-20 00:13:14 -0500

    first commit

---
+++ Grading Students/Grading Students/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grading_Students
+{
+    class Program
+    {
+        static List<int> gradingstudents(List<int> g)
+
+        {
+            List<int> grad = new List<int>();
+            for (int k = 0; k < g.Count; k++)
+                grad.Add(0);
+            for (int i = 0; i < g.Count; i++)
+                if (g[i] >= 38 && g[i] % 5 > 2)
+                    grad[i] = (g[i] - (g[i] % 5))+5;
+                else
+                    grad[i] = g[i];
+            return grad;
+                    
+        }
+        static void Main(string[] args)
+        {
+             
+                }
+
+    }
+}
