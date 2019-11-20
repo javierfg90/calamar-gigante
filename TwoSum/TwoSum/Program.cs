0f79f021ff1fc29fdc7c6055d3e028f039a29bb3
Javier <javierfg90@nauta.cu>
2019-11-20 00:13:44 -0500

    first commit

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    class Program
+    {
+        public static int[] TwoSum(int[] a, int target)
+        {
+            for (int k = 0; k < a.Length; k++)
+            {
+                for (int l = k + 1; l < a.Length; l++)
+                {
+                    if (a[k] + a[l] == target)
+                    {
+                        return new int[] { k, l };
+                    }
+                }
+            }
+            throw new Exception("No two sum solution");
+        }
+                    
+        static void Main(string[] args)
+        {
+            int[] a = { 2, 3, 5, 7, 11, 29, 31, 47, 79, 19, 17, 67,43,83 };
+            Console.WriteLine(TwoSum(a, 100));
+        }
+    }
+}
