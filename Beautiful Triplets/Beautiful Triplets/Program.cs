5aca9a1e6ef1e2bb57afa3cccadb26d3cd6eb1d3
Javier <javierfg90@nauta.cu>
2019-11-20 00:03:35 -0500

    first commit

---
+++ Beautiful Triplets/Beautiful Triplets/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beautiful_Triplets
+{
+    class Program
+    {
+        static int beautifulTriplets(int d, int[] arr)
+        {
+            int b = 0;
+            for (int i = 0; i < arr.Length - 2; i++)
+                for (int j = i + 1; j < arr.Length - 1; j++)
+                    if (arr[j] - arr[i] == d)
+                        for (int k = j + 1; k < arr.Length; k++)
+                            if (arr[k] - arr[j] == d)
+                                b += 1;
+            return b;
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] d = { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(beautifulTriplets(2,d));
+        }
+    }
+}
