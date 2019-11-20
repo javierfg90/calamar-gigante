fee5d6de7df679fc1fd554cd5cbb621c27305b52
Javier <javierfg90@nauta.cu>
2019-11-20 00:00:03 -0500

    first commit

---
+++ Angry Professor/Angry Professor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angry_Professor
+{
+    class Program
+    {
+        static string angryProfessor(int k, int[] a)
+        {
+            int ontime = 0;
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] <= 0)
+                    ontime += 1;
+            if (ontime >= k)
+                return "NO";
+            else
+                return "YES";
+        }
+        static void Main(string[] args)
+        {
+            int[] b = { 0, -1, 2, 1 };
+            Console.WriteLine(angryProfessor(2, b));
+        }
+    }
+}
