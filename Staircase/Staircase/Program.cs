036f7484ff617ecf9fd58dd7323ba22e4a21a774
Javier <javierfg90@nauta.cu>
2019-11-20 00:12:40 -0500

    first commit

---
+++ Staircase/Staircase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Staircase(int n)
+        {
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(new string('#', i + 1).PadLeft(n,' '));
+        }
+        static void Main(string[] args)
+        {
+            Staircase(4);
+        }
+    }
+}
